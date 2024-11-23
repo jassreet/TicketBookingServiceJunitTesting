1aefc62318dec7f4e18a806fed51c1336392effd
Jaspreet Singh <jaspreetsingh9102000@gmail.com>
2024-11-23 00:48:34 -0500

    pom

--- src/main/java/com/infotech/book/ticket/app/dao/TicketBookingDao.java
+++ src/main/java/com/infotech/book/ticket/app/dao/TicketBookingDao.java
@@ -1,9 +1,9 @@
 package com.infotech.book.ticket.app.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;  // Use JpaRepository instead of CrudRepository
 
 import com.infotech.book.ticket.app.entities.Ticket;
 
-public interface TicketBookingDao extends CrudRepository<Ticket, Integer>{
-	Ticket findByEmail(String email);
+public interface TicketBookingDao extends JpaRepository<Ticket, Integer> {
+    Ticket findByEmail(String email);  // Custom query to find ticket by email
 }
--- src/main/java/com/infotech/book/ticket/app/service/TicketBookingService.java
+++ src/main/java/com/infotech/book/ticket/app/service/TicketBookingService.java
@@ -6,31 +6,53 @@ import org.springframework.stereotype.Service;
 import com.infotech.book.ticket.app.dao.TicketBookingDao;
 import com.infotech.book.ticket.app.entities.Ticket;
 
+import java.util.Optional;
+
 @Service
 public class TicketBookingService {
 
-	@Autowired
-	private TicketBookingDao ticketBookingDao;
-	
-	public Ticket createTicket(Ticket ticket) {
-		return ticketBookingDao.save(ticket);
-	}
-	public Ticket getTicketById(Integer ticketId) {
-		return ticketBookingDao.findOne(ticketId);
-	}
-	public Iterable<Ticket> getAllBookedTickets() {
-		return ticketBookingDao.findAll();
-	}
-	public void deleteTicket(Integer ticketId) {
-		ticketBookingDao.delete(ticketId);
-	}
-	public Ticket updateTicket(Integer ticketId, String newEmail) {
-		Ticket ticketFromDb = ticketBookingDao.findOne(ticketId);
-		ticketFromDb.setEmail(newEmail);
-		Ticket upadedTicket = ticketBookingDao.save(ticketFromDb);
-		return upadedTicket;
-	}
-	public Ticket getTicketByEmail(String email) {
-		return ticketBookingDao.findByEmail(email);
-	}
+    @Autowired
+    private TicketBookingDao ticketBookingDao;
+    
+    // Create a new ticket
+    public Ticket createTicket(Ticket ticket) {
+        return ticketBookingDao.save(ticket);
+    }
+
+    // Get a ticket by its ID (using findById)
+    public Ticket getTicketById(Integer ticketId) {
+        Optional<Ticket> ticketOptional = ticketBookingDao.findById(ticketId); // Use findById instead of findOne
+        if (ticketOptional.isPresent()) {
+            return ticketOptional.get(); // Return the ticket if found
+        } else {
+            throw new RuntimeException("Ticket not found with id " + ticketId); // Handle not found scenario
+        }
+    }
+
+    // Get all booked tickets
+    public Iterable<Ticket> getAllBookedTickets() {
+        return ticketBookingDao.findAll();
+    }
+
+    // Delete a ticket by ID (using deleteById)
+    public void deleteTicket(Integer ticketId) {
+        ticketBookingDao.deleteById(ticketId); // Use deleteById instead of delete
+    }
+
+    // Update an existing ticket's email
+    public Ticket updateTicket(Integer ticketId, String newEmail) {
+        Optional<Ticket> ticketOptional = ticketBookingDao.findById(ticketId); // Use findById instead of findOne
+        if (ticketOptional.isPresent()) {
+            Ticket ticketFromDb = ticketOptional.get();
+            ticketFromDb.setEmail(newEmail); // Update email
+            return ticketBookingDao.save(ticketFromDb); // Save updated ticket
+        } else {
+            throw new RuntimeException("Ticket not found with id " + ticketId); // Handle not found scenario
+        }
+    }
+
+    // Get a ticket by email
+    public Ticket getTicketByEmail(String email) {
+        return ticketBookingDao.findByEmail(email);
+    }
 }
